f9a0f4a457a8e7b52a9d6ce9abc8b07c4861d6f6
Anvar Aliyev <aliyeva4381@gmail.com>
2022-10-05 15:04:30 +0500

    Bot deploying

--- bot.js
+++ bot.js
@@ -6,15 +6,18 @@ const fetch = require("node-fetch");
 bot.command("start", async (ctx) => {
   try {
     // Registering user to DB
-    const response = await fetch("http://localhost:1234/api/v1/user", {
-      method: "POST",
-      body: JSON.stringify({
-        first_name: ctx.from.first_name,
-        last_name: ctx.from.last_name,
-        username: ctx.from.username,
-      }),
-      headers: { "Content-Type": "application/json", chat_id: ctx.chat.id },
-    });
+    const response = await fetch(
+      "https://durgerking-bot.herokuapp.com/api/v1/user",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          first_name: ctx.from.first_name,
+          last_name: ctx.from.last_name,
+          username: ctx.from.username,
+        }),
+        headers: { "Content-Type": "application/json", chat_id: ctx.chat.id },
+      }
+    );
     console.log(await response.json());
   } catch (error) {
     return new Error(error);
