b17d0a27979dbbf71992225e25d3dbd38dd81587
Anvar Aliyev <aliyeva4381@gmail.com>
2022-10-05 14:55:40 +0500

    some

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ require("dotenv").config({ path: "./.env" });
 require("./config/db");
 
 app.listen(
-  (PORT = process.env.SERVER_PORT || 1234)
+  (PORT = process.env.PORT || 1234)
   // (URL = process.env.SERVER_URL),
   // (err) =>
   //   err ? console.log(err) : console.log(`Server is listening on ${PORT}`)
